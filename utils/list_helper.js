8cb81171be1d81ef15e38497afe185a5b06b5b30
Mykhailo Protsyshen <protsyshenm@gmail.com>
2023-08-03 00:55:58 +0300

    Solution for exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,8 +8,29 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const reducer = (mostLikedBlog, blog) => {
+    return blog.likes > mostLikedBlog.likes
+      ? blog
+      : mostLikedBlog
+  }
+
+  const mostLikedBlog = blogs.reduce(reducer, blogs[0])
+
+  return {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes
+  }
+} 
+
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
