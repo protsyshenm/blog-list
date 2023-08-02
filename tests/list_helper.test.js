3a7c63345ede4df652891f02416b7a96e90a43ba
Mykhailo Protsyshen <protsyshenm@gmail.com>
2023-08-02 23:51:04 +0300

    Solution for exercise 4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,65 @@ test('dummy returns 1', () => {
 
   expect(result).toBe(1)
 })
+
+describe('totalLikes', () => {
+
+  test('of empty list is 0', () => {
+    const blogs = []
+
+    const result = listHelper.totalLikes(blogs)
+
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const blogs = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      {
+        _id: '64363463634636436346436436346',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: '5a422aa464643671b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 7,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 6,
+        __v: 0
+      }
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+
+    expect(result).toBe(18)
+  })
+
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
