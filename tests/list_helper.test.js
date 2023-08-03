7034a3af35b7b35ca8d7fa4a8a92d073fac82031
Mykhailo Protsyshen <protsyshenm@gmail.com>
2023-08-03 18:10:10 +0300

    Solution for exercise 4.6

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -126,7 +126,7 @@ describe('favoriteBlog', () => {
     expect(result).toEqual(expectedResult)
   })
 
-  test('of a bigger list is finds right one', () => {
+  test('of a bigger list finds right one', () => {
     const blogs = [
       {
         _id: "5a422a851b54a676234d17f7",
@@ -184,6 +184,102 @@ describe('favoriteBlog', () => {
       title: "Canonical string reduction",
       author: "Edsger W. Dijkstra",
       likes: 12
+    }
+
+    expect(result).toEqual(expectedResult)
+  })
+
+})
+
+describe('mostBlogs', () => {
+
+  test('of empty list is null', () => {
+    const blogs = []
+
+    const result = listHelper.mostBlogs(blogs)
+
+    expect(result).toBe(null)
+  })
+
+  test('of one blog returns that blog`s author and blogs: 1' , () => {
+    const blogs = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+
+    const result = listHelper.mostBlogs(blogs)
+
+    const expectedResult = {
+      author: 'Edsger W. Dijkstra',
+      blogs: 1
+    }
+
+    expect(result).toEqual(expectedResult)
+  })
+
+  test('of many blogs returns correct author with most blogs and quantity of his blogs', () => {
+    const blogs = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+      },
+      {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+      },
+      {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
+      },
+      {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+      }
+    ]
+
+    const result = listHelper.mostBlogs(blogs)
+
+    const expectedResult = {
+      author: "Robert C. Martin",
+      blogs: 3
     }
 
     expect(result).toEqual(expectedResult)
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,14 +1,16 @@
-const dummy = (blogs) => {
+const _ = require('lodash')
+
+const dummy = blogs => {
   return 1
 }
 
-const totalLikes = (blogs) => {
+const totalLikes = blogs => {
   return blogs.reduce((sum, blog) => {
     return sum + blog.likes
   }, 0)
 }
 
-const favoriteBlog = (blogs) => {
+const favoriteBlog = blogs => {
   if (blogs.length === 0) {
     return null
   }
@@ -26,11 +28,37 @@ const favoriteBlog = (blogs) => {
     author: mostLikedBlog.author,
     likes: mostLikedBlog.likes
   }
-} 
+}
 
+const mostBlogs = blogs => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const groupedBlogs = _.groupBy(blogs, blog => {
+    return blog.author
+  })
+
+  let author = ''
+  let maxBlogsQuantity = 0
+  
+  for (const authorName in groupedBlogs) {
+    const blogsQuantity = groupedBlogs[authorName].length
+    if (blogsQuantity > maxBlogsQuantity) {
+      maxBlogsQuantity = blogsQuantity
+      author = authorName
+    }
+  }
+
+  return {
+    author: author,
+    blogs: maxBlogsQuantity
+  }
+}
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
