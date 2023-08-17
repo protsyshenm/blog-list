63e53cb71bb7640b78c69a64c09448bcce80e6a6
Mykhailo Protsyshen <protsyshenm@gmail.com>
2023-08-17 20:58:39 +0300

    Solution for exercise 4.15

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
@@ -12,5 +13,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
