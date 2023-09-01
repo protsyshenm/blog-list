9873c0293cab452e5c584f3287e4d94bd97bb4ca
Mykhailo Protsyshen <protsyshenm@gmail.com>
2023-09-01 20:00:14 +0300

    Solution for exercise 4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,15 +3,6 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
   response.json(blogs)
@@ -20,14 +11,14 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  if (!body.title || !body.url) {
+  if (!body.title || !body.url || !body.author)  {
     return response.status(400).json({ error: 'Missing content'})
   }
   if (!body.likes) {
     body.likes = 0
   }
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
